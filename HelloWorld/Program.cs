941c22aabb2c327c746bb830d709f3f615a1e3df
Tyler Ginn <tylerginn878@gmail.com>
2026-09-07 14:11:43 +0100

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter the first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the third Number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int sum = num1 * num2 + num3;
+
+Console.WriteLine("The total is: " + sum);
+Console.ReadLine(); // Keeps the window open
